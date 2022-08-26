0690fc7315f1f5b669e59686513802bc427416fc
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-08-26 11:24:51 +0200

    circles, tailwind minimized

--- components/SubHeader/SubHeader.jsx
+++ components/SubHeader/SubHeader.jsx
@@ -4,9 +4,10 @@ import Button from "../UI/Button";
 
 const SubHeader = () => {
   return (
-    <section className="overflow-hidden px-6 pt-20 bg-peach h-[843px] lg:h-[640px] md:px-20 md:w-11/12 md:mx-auto md:rounded-2xl lg:max-w-[1111px] lg:flex">
+    <section className="relative overflow-hidden px-6 pt-20 bg-peach h-[843px] lg:h-[640px] md:px-20 md:w-11/12 md:mx-auto md:rounded-2xl lg:max-w-[1111px] lg:flex">
+      <div className="absolute overflow-hidden w-full h-full ml-24 mt-14 bg-[url('/assets/home/desktop/bg-pattern-hero-home.svg')] md:h-[640px] md:w-2/4 md:mt-10 md:right-0 lg:w-2/4 lg:top-0 lg:right-0"></div>
       <div className="text-center text-white lg:text-left lg:mt-20 lg:w-2/4">
-        <h1 className="text-3xl md:text-5xl leading-9 font-medium">
+        <h1 className="text-3xl md:text-5xl leading-9 font-medium z-20">
           Award-winning custom designs and digital branding solutions
         </h1>
         <p className="my-3.5 md:my-5 lg:my-6 leading-6 text-[15px] md:text-base md:mx-18">
