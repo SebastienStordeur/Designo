25189184c8895e3e03f0a5728a0e13230b131655
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-08-26 13:59:48 +0200

    learn more button fix link

--- components/SubHeader/SubHeader.jsx
+++ components/SubHeader/SubHeader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import Button from "../UI/Button";
+import Link from "next/link";
 
 const SubHeader = () => {
   return (
     <section className="relative overflow-hidden px-6 pt-20 bg-peach h-[843px] lg:h-[640px] md:px-20 md:w-11/12 md:mx-auto md:rounded-2xl lg:max-w-[1111px] lg:flex">
-      <div className="absolute overflow-hidden w-full h-full ml-24 mt-14 bg-[url('/assets/home/desktop/bg-pattern-hero-home.svg')] md:h-[640px] md:w-2/4 md:mt-10 md:right-0 lg:w-2/4 lg:top-0 lg:right-0"></div>
       <div className="text-center text-white lg:text-left lg:mt-20 lg:w-2/4">
         <h1 className="text-3xl md:text-5xl leading-9 font-medium z-20">
           Award-winning custom designs and digital branding solutions
@@ -15,11 +15,13 @@ const SubHeader = () => {
           fully responsive websites, app design, and engaging brand experiences.
           Find out more about our services.
         </p>
-        <Button className="bg-white text-dark-grey hover:bg-light-peach hover:text-white">
-          Learn More
+        <Button className="bg-white text-dark-grey hover:bg-light-peach hover:text-white z-[1000]">
+          <Link href="/about" passHref>
+            Learn More
+          </Link>
         </Button>
       </div>
-      <div className="relative w-72 mx-auto h-[432px] scale-[2] mt-40 lg:scale-[2] lg:w-2/6 lg:mt-38">
+      <div className="relative w-72 mx-auto h-[432px] scale-[2] mt-48 lg:scale-[2] lg:w-2/6 lg:mt-38">
         <Image
           src="/assets/home/desktop/image-hero-phone.png"
           alt="Phone Image"
